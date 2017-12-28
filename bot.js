bdedddaeadfc3e4984befb570422620ec3f443d1
Daniel Carmona <DaniiCarmona22@gmail.com>
2017-12-28 23:04:08 +0100

    1st commit

--- bot.js
+++ bot.js
@@ -4,5 +4,14 @@ const client = new Discord.Client();
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.username}!`);
 });
+const p = "$sb";
+client.on('message', msg => {
+    
+        if(msg.content === (p + "Input")) {
+               msg.channel.sendMessage("Output");
+} 
+});
 
-client.login('shipbot');
+client.login('Mzk2MDUzMjUxMjM1MjUwMTk5.DSb1Hw.8rie__ih4z2fq14RvQapR2gCvYM');
+/*Client ID: 396053251235250199*/
+/*Uq3vXoKuSsvGn1y1M4iHPpT6pDUmRCG7*/
